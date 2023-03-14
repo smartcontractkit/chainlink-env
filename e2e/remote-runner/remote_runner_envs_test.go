c03a7b54bb9988e83f117dda9337ff73f15411e4
Tate <tate.exon@smartcontract.com>
2023-03-14 12:57:07 -0600

    Verify sub t.Runs work with remote runner (#365)
    * Try out t.Run that rely on parent test
    
    * Cleanup and add more comments

--- e2e/remote-runner/remote_runner_envs_test.go
+++ e2e/remote-runner/remote_runner_envs_test.go
@@ -5,9 +5,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/rs/zerolog/log"
 	"github.com/smartcontractkit/chainlink-env/e2e/common"
 	"github.com/smartcontractkit/chainlink-env/environment"
+	"github.com/smartcontractkit/chainlink-env/pkg/helm/chainlink"
+	"github.com/smartcontractkit/chainlink-env/pkg/helm/ethereum"
+	"github.com/smartcontractkit/chainlink-env/pkg/helm/mockserver"
+	mockservercfg "github.com/smartcontractkit/chainlink-env/pkg/helm/mockserver-cfg"
 	"github.com/smartcontractkit/chainlink-env/presets"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -68,7 +73,14 @@ func TestFundReturnShutdownLogic(t *testing.T) {
 func TestRemoteRunnerOneSetupWithMultipeTests(t *testing.T) {
 	t.Parallel()
 	testEnvConfig := common.GetTestEnvConfig(t)
-	e := presets.EVMMinimalLocal(testEnvConfig)
+	ethChart := ethereum.New(nil)
+	e := environment.New(testEnvConfig).
+		AddHelm(mockservercfg.New(nil)).
+		AddHelm(mockserver.New(nil)).
+		AddHelm(ethChart).
+		AddHelm(chainlink.New(0, map[string]interface{}{
+			"replicas": 5,
+		}))
 	err := e.Run()
 	t.Cleanup(func() {
 		assert.NoError(t, e.Shutdown())
@@ -78,14 +90,44 @@ func TestRemoteRunnerOneSetupWithMultipeTests(t *testing.T) {
 		return
 	}
 
+	// setup of variables to use for verification in a t.Run
+	ethNetworkName := ethChart.GetProps().(*ethereum.Props).NetworkName
+	urls := make([]string, 0)
+	if e.Cfg.InsideK8s {
+		urls = append(urls, e.URLs[chainlink.NodesInternalURLsKey]...)
+		urls = append(urls, e.URLs[ethNetworkName+"_internal_http"]...)
+	} else {
+		urls = append(urls, e.URLs[chainlink.NodesLocalURLsKey]...)
+		urls = append(urls, e.URLs[ethNetworkName+"_http"]...)
+	}
+
 	log.Info().Str("Test", "Before").Msg("Before Tests")
-	t.Run("do one", func(t *testing.T) {
+
+	// This test will verify we can connect a t.Run to the env of the parent test
+	t.Run("do one", func(t1 *testing.T) {
+		t1.Parallel()
+		test1EnvConfig := common.GetTestEnvConfig(t1)
+		test1EnvConfig.Namespace = e.Cfg.Namespace
+		test1EnvConfig.NoManifestUpdate = true
+		e1 := presets.EVMMinimalLocal(test1EnvConfig)
+		err := e1.Run()
+		require.NoError(t1, err)
 		log.Info().Str("Test", "One").Msg("Inside test")
 		time.Sleep(1 * time.Second)
 	})
-	t.Run("do two", func(t *testing.T) {
+
+	// This test will verify the sub t.Run properly uses the variables from the parent test
+	t.Run("do two", func(t2 *testing.T) {
+		t2.Parallel()
 		log.Info().Str("Test", "Two").Msg("Inside test")
-		time.Sleep(1 * time.Second)
+		r := resty.New()
+		for _, u := range urls {
+			log.Info().Str("URL", u).Send()
+			res, err := r.R().Get(u)
+			require.NoError(t2, err)
+			require.Equal(t2, "200 OK", res.Status())
+		}
 	})
+
 	log.Info().Str("Test", "After").Msg("After Tests")
 }
