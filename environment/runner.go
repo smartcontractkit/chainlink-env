e1e035e427097fcb3e6534b99d307efda8447e52
Tate <tate.exon@smartcontract.com>
2023-04-06 14:46:09 -0600

    Move remote runner environment logic to remote runner Fix logic to use prefixed variables over non prefixed so when you actively set a prfixed one you get your deterministic result.

--- environment/runner.go
+++ environment/runner.go
@@ -3,8 +3,11 @@ package environment
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
+	"github.com/rs/zerolog/log"
 	"github.com/smartcontractkit/chainlink-env/config"
 	"github.com/smartcontractkit/chainlink-env/imports/k8s"
 	a "github.com/smartcontractkit/chainlink-env/pkg/alias"
@@ -54,11 +57,12 @@ func NewRunner(props *Props) func(root cdk8s.Chart) ConnectedChart {
 }
 
 type Props struct {
-	BaseName        string
-	TargetNamespace string
-	Labels          *map[string]*string
-	Image           string
-	EnvVars         map[string]string
+	BaseName         string
+	TargetNamespace  string
+	Labels           *map[string]*string
+	Image            string
+	TestName         string
+	NoManifestUpdate bool
 }
 
 func role(chart cdk8s.Chart, props *Props) {
@@ -158,9 +162,60 @@ func container(props *Props) *k8s.Container {
 }
 
 func jobEnvVars(props *Props) *[]*k8s.EnvVar {
+	// Use a map to set values so we can easily overwrite duplicate values
+	env := make(map[string]string)
+
+	// Propagate common environment variables to the runner
+	lookups := []string{
+		config.EnvVarCLImage,
+		config.EnvVarCLTag,
+		config.EnvVarCLCommitSha,
+		config.EnvVarLogLevel,
+		config.EnvVarTestTrigger,
+		config.EnvVarToleration,
+		config.EnvVarSlackChannel,
+		config.EnvVarSlackKey,
+		config.EnvVarSlackUser,
+		config.EnvVarPyroscopeKey,
+		config.EnvVarPyroscopeEnvironment,
+		config.EnvVarPyroscopeServer,
+		config.EnvVarUser,
+		config.EnvVarNodeSelector,
+		config.EnvVarSelectedNetworks,
+		config.EnvVarDBURL,
+		config.EnvVarEVMKeys,
+		config.EnvVarInternalDockerRepo,
+		config.EnvVarEVMUrls,
+		config.EnvVarEVMHttpUrls,
+	}
+	for _, k := range lookups {
+		v, success := os.LookupEnv(k)
+		if success && len(v) > 0 {
+			log.Debug().Str(k, v).Msg("Forwarding Env Var")
+			env[k] = v
+		}
+	}
+
+	// Propagate prefixed variables to the runner
+	// These should overwrite anything that was unprefixed if they match up
+	for _, e := range os.Environ() {
+		if i := strings.Index(e, "="); i >= 0 {
+			if strings.HasPrefix(e[:i], config.EnvVarPrefix) {
+				withoutPrefix := strings.Replace(e[:i], config.EnvVarPrefix, "", 1)
+				env[withoutPrefix] = e[i+1:]
+			}
+		}
+	}
+
+	// Add variables that should need specific values for thre remote runner
+	env[config.EnvVarNamespace] = props.TargetNamespace
+	env["TEST_NAME"] = props.TestName
+	env[config.EnvVarInsideK8s] = "true"
+	env[config.EnvVarNoManifestUpdate] = strconv.FormatBool(props.NoManifestUpdate)
+
+	// convert from map to the expected array
 	cdk8sVars := make([]*k8s.EnvVar, 0)
-	cdk8sVars = append(cdk8sVars, a.EnvVarStr(config.EnvVarNamespace, props.TargetNamespace))
-	for k, v := range props.EnvVars {
+	for k, v := range env {
 		cdk8sVars = append(cdk8sVars, a.EnvVarStr(k, v))
 	}
 	return &cdk8sVars
