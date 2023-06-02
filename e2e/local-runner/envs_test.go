4ec0bd3490e0b657253e3e5193bd24597b23c328
Tate <tate.exon@smartcontract.com>
2023-06-02 13:39:39 -0600

    The mercury-server is being moved to its own repo (#450)

--- e2e/local-runner/envs_test.go
+++ e2e/local-runner/envs_test.go
@@ -1,37 +1,10 @@
 package env_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/smartcontractkit/chainlink-env/e2e/common"
-	"github.com/smartcontractkit/chainlink-env/environment"
-	mercury_server "github.com/smartcontractkit/chainlink-env/pkg/helm/mercury-server"
-	"github.com/stretchr/testify/require"
 )
-
-func TestOCIChart(t *testing.T) {
-	t.Parallel()
-	url := "oci://my-erc/my-repo"
-	ver := "v1.0.8"
-	t.Run("invalid URL", func(t *testing.T) {
-		t.Parallel()
-		err := environment.New(nil).
-			AddHelm(mercury_server.New(url, ver, nil)).
-			Run()
-		require.Error(t, err)
-		require.ErrorContains(t, err, fmt.Sprintf(environment.ErrInvalidOCI, url), "The error is not ErrInvalidOCI")
-	})
-	t.Run("failed to pull a valid URL", func(t *testing.T) {
-		t.Parallel()
-		url := "oci://my-erc/my-repo/my-chart"
-		err := environment.New(nil).
-			AddHelm(mercury_server.New(url, ver, nil)).
-			Run()
-		require.Error(t, err)
-		require.ErrorContains(t, err, fmt.Sprintf(environment.ErrOCIPull, url), "The error is not ErrInvalidOCI")
-	})
-}
 
 func TestMultiStageMultiManifestConnection(t *testing.T) {
 	common.TestMultiStageMultiManifestConnection(t)
