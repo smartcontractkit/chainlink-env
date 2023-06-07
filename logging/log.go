42bb02373b4f8e724eca8edb2397f74f3f5282d4
Tate <tate.exon@smartcontract.com>
2023-06-07 08:51:30 -0600

    Remove duplicated timestamps when possible

--- logging/log.go
+++ logging/log.go
@@ -13,7 +13,7 @@ import (
 // People often call this multiple times
 var loggingMu sync.Mutex
 
-func Init(t *testing.T) {
+func Init(t *testing.T, withTimestamp bool) {
 	loggingMu.Lock()
 	defer loggingMu.Unlock()
 	lvlStr := os.Getenv(config.EnvVarLogLevel)
@@ -24,10 +24,14 @@ func Init(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+
 	// use the test logger if t is set
 	if t != nil {
-		log.Logger = zerolog.New(zerolog.NewTestWriter(t)).Output(zerolog.ConsoleWriter{Out: os.Stderr}).Level(lvl).With().Timestamp().Logger()
+		log.Logger = zerolog.New(zerolog.NewTestWriter(t)).Output(zerolog.ConsoleWriter{Out: os.Stderr}).Level(lvl)
 	} else {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).Level(lvl)
+	}
+	if withTimestamp {
+		log.Logger = log.Logger.With().Timestamp().Logger()
 	}
 }
