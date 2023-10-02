01d9b36160400d9802b78e917f3d6d371843bf05
Tate <tate.exon@smartcontract.com>
2023-10-02 11:14:18 -0600

    Remove more preventative measures for pod eviction by default

--- environment/environment.go
+++ environment/environment.go
@@ -285,21 +285,23 @@ func (m *Environment) initApp() error {
 			Annotations: &defaultNamespaceAnnotations,
 		},
 	})
-	zero := float64(0)
-	k8s.NewKubePodDisruptionBudget(m.root, a.Str("pdb"), &k8s.KubePodDisruptionBudgetProps{
-		Metadata: &k8s.ObjectMeta{
-			Name:      a.Str("clenv-pdb"),
-			Namespace: a.Str(m.Cfg.Namespace),
-		},
-		Spec: &k8s.PodDisruptionBudgetSpec{
-			MaxUnavailable: k8s.IntOrString_FromNumber(&zero),
-			Selector: &k8s.LabelSelector{
-				MatchLabels: &map[string]*string{
-					pkg.NamespaceLabelKey: a.Str(m.Cfg.Namespace),
+	if m.Cfg.PreventPodEviction {
+		zero := float64(0)
+		k8s.NewKubePodDisruptionBudget(m.root, a.Str("pdb"), &k8s.KubePodDisruptionBudgetProps{
+			Metadata: &k8s.ObjectMeta{
+				Name:      a.Str("clenv-pdb"),
+				Namespace: a.Str(m.Cfg.Namespace),
+			},
+			Spec: &k8s.PodDisruptionBudgetSpec{
+				MaxUnavailable: k8s.IntOrString_FromNumber(&zero),
+				Selector: &k8s.LabelSelector{
+					MatchLabels: &map[string]*string{
+						pkg.NamespaceLabelKey: a.Str(m.Cfg.Namespace),
+					},
 				},
 			},
-		},
-	})
+		})
+	}
 	m.CurrentManifest = *m.App.SynthYaml()
 	// loop retry applying the initial manifest with the namespace and other basics
 	ctx, cancel := context.WithTimeout(context.Background(), m.Cfg.ReadyCheckData.Timeout)
